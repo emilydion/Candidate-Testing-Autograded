4a7897df1ac24ea2ef21013206dbfc556320ad7b
emilydion <edion468@gmail.com>
2024-01-29 17:42:41 -0600

    removed call to runProgram() in main file

--- candidate-testing.js
+++ candidate-testing.js
@@ -64,9 +64,6 @@ function runProgram() {
   gradeQuiz(candidateAnswers);
 }
 
-runProgram(); 
-
-
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
